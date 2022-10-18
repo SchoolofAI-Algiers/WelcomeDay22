3946383fa8a41634f947c8b16ef74a3e47cd7ae8
Abderaouf Louggani <ja_louggani@esi.dz>
2022-10-18 22:41:19 +0100

    fixing responsivity

--- src/components/ChallengeCard.jsx
+++ src/components/ChallengeCard.jsx
@@ -1,21 +1,33 @@
-export default function ChallengeCard() {
+export default function ChallengeCard({ setChallengeID, id, selected, image }) {
   return (
-    <div className="relative img-card duration-1000">
+    <div className="relative img-card my-4" onClick={() => setChallengeID(id)}>
+      <div
+        id="highlight"
+        className={`absolute ${
+          selected ? "w-0 h-0" : "h-72 w-72"
+        } bg-[#6FCBDC] blur-[120px] hidden`}
+      />
       <div
         id="first-border"
         className="absolute border-[0.5px] top-0 right-0 border-[#6FCBDC] h-full w-full"
       />
       <div
         id="second-border"
-        className="absolute border-[0.5px] top-1 right-1 border-[#6FCBDC] h-full w-full"
+        className={`absolute border-[0.5px] ${
+          selected ? "top-0 right-0" : "top-1 right-1"
+        } border-[#6FCBDC] h-full w-full`}
       />
       <div
         id="third-border"
-        className="absolute border-[0.5px] top-2 right-2 border-[#6FCBDC] h-full w-full"
+        className={`absolute border-[0.5px] ${
+          selected ? "top-0 right-0" : "top-2 right-2"
+        } border-[#6FCBDC] h-full w-full`}
       />
       <img
-        className="w-auto h-96 relative top-3 right-3"
-        src="https://cdn.shopify.com/s/files/1/0747/3829/products/mL4465_1024x1024.jpg?v=1591827152"
+        className={`w-auto h-60 md:h-72 lg:h-96 relative ${
+          selected ? "top-0 right-0" : "top-3 right-3"
+        }`}
+        src={image}
       />
     </div>
   );
--- src/sections/Challenges.jsx
+++ src/sections/Challenges.jsx
@@ -1,41 +1,77 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import ChallengeCard from "../components/ChallengeCard";
 
 export default function Challenges() {
+  const ref = useRef();
+  const [username, setUsername] = useState("");
   const [challengeID, setChallengeID] = useState();
   const challenges = [
     {
       id: 1,
+      image:
+        "https://cdn.shopify.com/s/files/1/0747/3829/products/mL4465_1024x1024.jpg?v=1591827152",
     },
     {
       id: 2,
+      image:
+        "https://cdn.shopify.com/s/files/1/0747/3829/products/mL4465_1024x1024.jpg?v=1591827152",
     },
     {
       id: 3,
+      image:
+        "https://cdn.shopify.com/s/files/1/0747/3829/products/mL4465_1024x1024.jpg?v=1591827152",
     },
   ];
 
   return (
-    <section className="background min-h-screen text-white p-16 overflow-hidden">
-      <div className="bg-[#FFCC00] h-48 w-48 blur-[100px] absolute top-0 -translate-x-1/2 left-1/2 -translate-y-1/2" />
+    <section className="background min-h-screen text-white p-6 md:p-16 overflow-hidden">
+      <div className="bg-[#FFCC00] h-36 w-36 md:h-48 md:w-48 blur-[60px] md:blur-[100px] absolute top-0 -translate-x-1/2 left-1/2 -translate-y-1/2" />
 
       <div className="relative w-full flex flex-col items-center">
-        <h1 className="font-arcade text-4xl">challenges</h1>
-        <div className="w-full flex flex-col items-start mt-24">
+        <h1 className="font-arcade text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+          challenges
+        </h1>
+        <div className="w-full flex flex-col items-start mt-8 md:mt-12 lg:mt-24">
           <div>
-            <div className="text-4xl font-arcade">
+            <div className="text-xl md:text-2xl lg:text-3xl xl:text-4xl font-arcade">
               <h2>let's do some</h2>
               <h2 className="text-[#E6A2C7]">challenges !</h2>
             </div>
           </div>
-          <h3 className="text-3xl mt-4">Play againt the AI! Choose a game</h3>
+          <h3 className="text-lg md:text-xl lg:text-2xl xl:text-3xl mt-4">
+            Play againt the AI! Choose a game
+          </h3>
         </div>
       </div>
 
-      <div className="flex flex-wrap justify-evenly my-16">
+      <div className="flex flex-wrap justify-evenly my-8">
         {challenges.map((challenge) => (
-          <ChallengeCard key={challenge.id} />
+          <ChallengeCard
+            key={challenge.id}
+            setChallengeID={setChallengeID}
+            id={challenge.id}
+            selected={challengeID === challenge.id}
+            image={challenge.image}
+          />
         ))}
+      </div>
+
+      <div className="flex justify-center font-arcade gap-x-8 text-xs md:text-sm flex-col md:flex-row">
+        <input
+          ref={ref}
+          className="text-white bg-transparent border-2 border-[#E6A2C7] outline-none rounded-sm p-2 my-2"
+          type="text"
+          placeholder="enter user name"
+        />
+        <button
+          onClick={() => {
+            setUsername(ref.current.value);
+          }}
+        >
+          <div className="bg-[#E6A2C7] p-2 my-2 md:p-4 rounded-sm">
+            start playing
+          </div>
+        </button>
       </div>
     </section>
   );
