b9e75629f5d08d277a3460abd4b679f964ffebc1
yassin <y.attuo@esi-sba.dz>
2022-10-18 16:29:40 +0200

    update2

--- src/pages/challenge.jsx
+++ src/pages/challenge.jsx
@@ -3,7 +3,7 @@ import  data  from './data.json'
 
 export default function Challenges() {
     const players = data;
-      
+    const [load,setload] = useState(true);  
       const [width, setWidth] = useState(128);
       if (typeof window != undefined) {
         // browser code
@@ -26,29 +26,46 @@ export default function Challenges() {
       }, []);
     
        return (
-    <div className='  min-h-screen w-full flex flex-col md:grid md:grid-cols-28 md:grid-rows-7 '>
-    <img src='/bgChallenges.png' className='w-full h-screen -z-10 absolute top-0 right-0 ' />
-    {/* LEADERBOARD */}
+    <>
 
-    <div className=' mt-24 md:mt-0 mx-8 md:mx-0 md:col-start-22 md:col-span-4  md:row-start-2 md:row-span-5  flex justify-start   flex-col items-center'>
-        <div className='w-9/12 h-8 lg:h-12 bg-LEADERBOARD 
-        rounded-md flex justify-center z-10 items-center '> 
-        <h3 className='text-white font-normal lg:font-semibold  text-xs md:text-base   xl:text-lg'>{width < breakpoint ?'LeaderBoard':'leadBoard'}</h3></div>
-        <div className ={`' w-full border-2 border-LEADERBOARD -mt-6 z-9 pt-8 h-full  flex flex-row space-x-2 md:flex-col flex-wrap'
-        }'
-         `}>
-        { players.sort((a,b) => b.points-a.points).map((player,rank) => (
 
-           <div key={player.id} className='text-white lg:pl-4 text-sm md:text-md xl:text-lg '> {`${rank+1} . ${player.name} ${width > breakpoint?player.points+'pts':''}`}</div>
-        ))
-        }
-        </div>
-    </div>
-    {/*game place */}
-    <div className='bg-rose-500 bg-opacity-25 flex-1 md:row-start-2 md:row-span-5 md:col-start-3 md:col-span-20  '>
+  
+          <div className='  min-h-screen w-full flex flex-col md:grid md:grid-cols-28 md:grid-rows-7 absolute top-0 left-0  z-10'>
+          <img src='/bgChallenges.png' className='w-full h-screen -z-10 absolute top-0 right-0 ' />
+          {/* LEADERBOARD */}
+      
+          <div className=' mt-24 md:mt-0 mx-8 md:mx-0 md:col-start-22 md:col-span-4 z-10 md:row-start-2 md:row-span-5  flex justify-start   flex-col items-center'>
+              <div className='w-9/12 h-8 lg:h-12 bg-LEADERBOARD 
+              rounded-md flex justify-center  items-center '> 
+              <h3 className='text-white font-normal lg:font-semibold z-10 text-xs md:text-base   xl:text-lg'>LeaderBoard</h3></div>
+              <div className ={`' w-full border-2 border-LEADERBOARD -mt-6 z-10 pt-8 h-full  flex flex-row space-x-2 md:flex-col flex-wrap'
+              }'
+               `}>
+              { players.sort((a,b) => b.points-a.points).map((player,rank) => (
+      
+                 <div key={player.id} className='text-white lg:pl-4 text-sm md:text-md xl:text-lg '> {`${rank+1} . ${player.name} ${width > breakpoint?player.points+'pts':''}`}</div>
+              ))
+              }
+              </div>
+          </div>
+          {/*game place */}
+          <div className='bg-rose-500 bg-opacity-25 flex-1 md:row-start-2 md:row-span-5 md:col-start-3 md:col-span-20  '>
+              
+          </div>
          
-    </div>
+          </div>
+
+          {  (
+  <div className='w-full flex h-screen overflow-hidden abolute top-0 left-0 z-50'>
+  <img src='bgChallenges.png' onClick={() =>{setload(!load)}} className={`w-1/2  z-40 
+  ${(!load)&& 'transition ease-in-out delay-400 duration-1000 translate-x-[-50rem]'}   `}   />
+   <img src='bgChallenges.png' className={`w-1/2  z-40 
+  ${(!load)&& 'transition ease-in-out delay-400 duration-1000 translate-x-[50rem]'}   `}   />
    
-    </div>
+  </div>
+)}
+             
+    </>
+    
   )
 }
