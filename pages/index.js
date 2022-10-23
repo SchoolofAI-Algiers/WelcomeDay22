c11d4ffe9c8f98ad9f25cc4e32f1a622bd93de59
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-10-23 02:43:09 +0100

    fix prob

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,13 @@ import Events from "../components/Events";
 import Contact from "../components/Contact";
 import Home from "../components/Home";
 import LeaderBoard from "../components/LeaderBord.jsx";
-import LeaderBoardChallenge from "./LeaderBoardChallenge.jsx";
 
 export default function App() {
   return (
     <div>
       <Home />
       <Events />
-      <LeaderBoard/>
+      <LeaderBoard />
       <Faq />
       <Contact />
     </div>
