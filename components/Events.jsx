c5f5f467cf7f1a54126af5980b3b3f88faa61a2c
moonayyur <jm_kias@esi.dz>
2022-10-23 01:30:01 +0100

    Fix events

--- components/Events.jsx
+++ components/Events.jsx
@@ -4,29 +4,29 @@ function Events() {
   const events = [
     {
       title: "Reading Sessions",
-      desc: "We discuss the content of a scientific paper",
+      desc: "Scientific paper content discussion",
       date: "Each Friday 7pm ",
       picture: "bg-event1",
       width: "md:w-5/12 w-full",
     },
     {
       title: "AI2E",
-      desc: "Full series of workshops ",
+      desc: "Series of workshops",
       date: "Each year",
       picture: "bg-event2",
       width: "md:w-6/12 w-full",
     },
     {
       title: "HAICK",
-      desc: "An AI hackathon",
+      desc: "AI hackathon",
       date: "Each year",
       picture: "bg-event3",
       width: "md:w-6/12 w-full",
     },
     {
-      title: "Guide d’achat",
-      desc: "30/12/2021",
-      date: "Les meilleurs ensembles Apple",
+      title: "AI Day",
+      desc: "Series of workshops and talks",
+      date: "Each year",
       picture: "bg-event4",
       width: "md:w-5/12 w-full",
     },
@@ -49,38 +49,36 @@ function Events() {
         <div className="bg-[#FFCC00] h-36 w-36 md:h-48 md:w-48 blur-[60px] md:blur-[100px] absolute top-0 -translate-x-1/2 left-1/2 -translate-y-1/2" />
 
         <div className="relative w-full flex flex-col items-center">
-          <h1 className=" text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+          <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl">
             Events
           </h1>
-          <div className="w-full  md:ml-20  flex flex-col items-start mt-8   md:mt-12 lg:mt-24">
+          <div className="w-full md:ml-20 flex flex-col items-start mt-8 md:mt-12 lg:mt-24">
             <p className="text-lg md:text-2xl lg:text-2xl xl:text-2xl mt-10">
-              What we do in SOIA?
+              What do we do in SOAI?
             </p>
-            <div className="lg:w-3/5  md:w-3/4 w-full">
-              <p className="lg:text-sm text-xs    md:max-w-2xl mt-4">
-                We organize a veriaty of amazing and exciting AI events, in
-                which we aim to help you to learn and develop in the field of
-                AI, all while having fun and enjoying our selfes
+            <div className="lg:w-3/5 md:w-3/4 w-full">
+              <p className="lg:text-sm text-xs lg:leading-6 leading-5 md:max-w-2xl mt-4">
+                Our main goal is to help each other enhance our knowledge and
+                improve our skills in the field by organizing various amazing AI
+                events
               </p>
             </div>
           </div>
         </div>
         {/* Body */}
-        <div className="   my-10">
-          <div className="md:flex  justify-around my-10 flex-wrap  w-full ">
-            {events.map((item, index) => {
-              return (
-                <ImageEvent
-                  key={index}
-                  src={item.picture}
-                  title={item.title}
-                  date={item.date}
-                  desc={item.desc}
-                  width={item.width}
-                />
-              );
-            })}
-          </div>
+        <div className="md:flex justify-center my-10 space-y-5 md:space-y-0 md:gap-5 flex-wrap w-full">
+          {events.map((item, index) => {
+            return (
+              <ImageEvent
+                key={index}
+                src={item.picture}
+                title={item.title}
+                date={item.date}
+                desc={item.desc}
+                width={item.width}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
--- components/ImageEvent.js
+++ components/ImageEvent.js
@@ -5,16 +5,14 @@ function ImageEvent({ src, title, date, desc, width }) {
     <div
       className={
         width +
-        " flex  items-end lg:h-96 md:h-80 h-64 rounded-2xl bg-cover md:my-2 my-8 pb-4 " +
+        "flex items-end lg:h-96 md:h-80 h-64 rounded-2xl bg-cover pb-4 " +
         src
       }
     >
-      <div className=" pl-6 ">
-        <p className=" text-white mb-1 text-sm font-normal ">
-          {title}
-        </p>
-        <p className=" text-white  text-xs font-thin">{date}</p>
-        <p className=" text-white text-sm font-normal mt-2  md:block hidden">
+      <div className="p-6">
+        <p className="text-white mb-1 text-sm font-normal">{title}</p>
+        <p className="text-white text-xs font-thin">{date}</p>
+        <p className="text-white text-sm font-normal mt-2 md:block hidden">
           {desc}
         </p>
       </div>
