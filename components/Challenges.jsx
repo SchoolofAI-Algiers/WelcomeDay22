8ae919c612b15feae2b3fcff70d30cbefb51e29d
moonayyur <jm_kias@esi.dz>
2022-10-23 02:05:10 +0100

    Fix challenges

--- components/Challenges.jsx
+++ components/Challenges.jsx
@@ -24,35 +24,41 @@ export default function Challenges() {
   ];
 
   return (
-    <section className="relative min-h-screen text-white p-6 md:p-16 overflow-hidden" id="Challenges">
-        <img
-          src="/assets/Group6.png"
-          className="invisible md:visible absolute xl:w-80 md:w-56 sm:w-36 w-28 top-5 left-0"
-        />
-        <img
-          src="/assets/Group5.png"
-          className="invisible md:visible absolute lg:w-80 md:w-56 sm:w-36 w-28 top-[-1] right-0"
-        />
+    <section
+      className="relative min-h-screen text-white p-6 md:p-16 overflow-hidden"
+      id="Challenges"
+    >
+      <img
+        src="/assets/Group6.png"
+        className="invisible md:visible absolute xl:w-80 md:w-56 sm:w-36 w-28 top-5 left-0"
+      />
+      <img
+        src="/assets/Group5.png"
+        className="invisible md:visible absolute lg:w-80 md:w-56 sm:w-36 w-28 top-[-1] right-0"
+      />
       <div className="bg-[#FFCC00] h-36 w-36 md:h-48 md:w-48 blur-[60px] md:blur-[100px] absolute top-0 -translate-x-1/2 left-1/2 -translate-y-1/2" />
 
       <div className="relative w-full flex flex-col items-center">
-        <h1 className=" text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+        <h1 className="text-xl md:text-2xl lg:text-3xl xl:text-4xl">
           challenges
         </h1>
-        <div className="w-full flex flex-col items-start mt-8 md:mt-12 lg:mt-24">
+        <div className="w-full flex flex-col lg:text-xl md:text-lg text-sm items-start mt-8 md:mt-12 lg:mt-24">
           <div>
-            <div className="lg:text-xl md:text-lg text-sm   ">
-              <h2>let's do some</h2>
-              <h2 className="text-[#E6A2C7] lg:text-2xl sm:text-xl">challenges !</h2>
+            <div>
+              <h2 className="flex flex-wrap items-center gap-x-4">
+                Let's do some
+                <h2 className="text-[#E6A2C7]">challenges !</h2>
+              </h2>
             </div>
           </div>
-          <h3 className="lg:text-xl md:text-lg text-sm   mt-4">
-            Play againt the AI! Choose a game
+          <h3 className="mt-4">
+            Play against the AI
+            <h3 className="text-[#E6A2C7]">Choose a game</h3>
           </h3>
         </div>
       </div>
 
-      <div className="flex flex-wrap justify-evenly my-8">
+      <div className="flex flex-wrap justify-evenly gap-x-6 my-8">
         {challenges.map((challenge) => (
           <ChallengeCard
             key={challenge.id}
@@ -64,7 +70,7 @@ export default function Challenges() {
         ))}
       </div>
 
-      <div className="flex justify-center  gap-x-8 text-xs md:text-sm flex-col md:flex-row">
+      <div className="flex justify-center gap-x-8 text-xs md:text-sm flex-col sm:flex-row">
         <input
           ref={ref}
           className="text-white bg-transparent border-2 border-[#E6A2C7] outline-none rounded-sm p-2 my-2"
